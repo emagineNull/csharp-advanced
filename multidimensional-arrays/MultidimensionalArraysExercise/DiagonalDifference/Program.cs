1012c65d25ee0c7f7a82de924f121b38630f5f50
star-lord <alekrifai97@gmail.com>
2019-10-12 19:41:27 +0300

    Processing Matrices and Jagged Arrays

---
+++ multidimensional-arrays/MultidimensionalArraysExercise/DiagonalDifference/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int primaryDiagonal = 0;
+            int secondaryDiagonal = 0;
+
+            int currentRowIndex = 0; 
+            int currentColIndex = 0; 
+
+            while (true)
+            {
+                if (currentRowIndex > size - 1)
+                {
+                    break;
+                }
+
+                primaryDiagonal += matrix[currentRowIndex, currentColIndex];
+
+                currentRowIndex++;
+                currentColIndex++;
+            }
+
+            currentRowIndex = 0;
+            currentColIndex = size - 1;
+
+            while (true)
+            {
+                if (currentRowIndex > size - 1)
+                {
+                    break;
+                }
+
+                secondaryDiagonal += matrix[currentRowIndex, currentColIndex];
+
+                currentRowIndex++;
+                currentColIndex--;
+            }
+
+            int difference = Math.Abs(primaryDiagonal - secondaryDiagonal);
+
+            Console.WriteLine(difference);
+        }
+    }
+}
---
+++ multidimensional-arrays/MultidimensionalArraysExercise/SquaresInMatrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] currentInput = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentInput[col];
+                }
+            }
+
+            int counter = 0;
+
+            char currentSquareCell0 = ' ';
+            char currentSquareCell1 = ' ';
+            char currentSquareCell2 = ' ';
+            char currentSquareCell3 = ' ';
+
+            int currentRow = 0;
+            int currentCol = 0;
+
+            //while (true)
+            //{
+            //    if (currentRow > rows - 1 || currentCol )
+            //    {
+
+            //    }
+            //}
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    int[,] currentMatrix = new int[2, 2];
+
+
+                }
+            }
+        }
+    }
+}
