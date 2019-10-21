158d974de9d5419f298008db5129e2797cfa8c08
star-lord <alekrifai97@gmail.com>
2019-10-21 22:44:42 +0300

    File Types, Using Streams and Manipulating Files

---
+++ streams-files-and-directories/StreamsFilesDirectoriesExercise/EvenLines/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("text.txt"))
+            {
+                using (var writer = File.CreateText("output.txt"))
+                {
+                    int counter = 0;
+
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        line = string.Join(" ", line.Split(' ').Reverse());
+                        string replaced = Regex.Replace(line, "[-|,|.|!|?]", "@");
+
+                        if (counter % 2 == 0)
+                        {
+                            writer.WriteLine(replaced);
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ streams-files-and-directories/StreamsFilesDirectoriesExercise/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
