9dc621d37b8675daeeb3bac683160c830f8d9869
star-lord <alekrifai97@gmail.com>
2019-10-27 06:11:05 +0200

    Functional Programming

---
+++ functional-programming/FunctionalProgrammingExercise/TriFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, int, bool> isGreater = (a, b) => a.Sum(c => c) >= b;
+
+            Func<Func<string, int, bool>, List<string>, string> returnFirst = (a, b) => b.FirstOrDefault(s => a(s, n));
+
+            string result = returnFirst(isGreater, names);
+
+            if (result != null)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(string.Empty);
+            }
+        }
+    }
+}
