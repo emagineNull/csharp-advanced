3857fa4673c68862ac13bf6535ddbd9521eb3566
star-lord <alekrifai97@gmail.com>
2019-11-30 20:54:27 +0200

    Adding Type Safety and Code Reusability

---
+++ generic/GenericsLab/GenericScale/EqualityScale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable<T>
+    {
+        private T right;
+        private T left;
+
+        public EqualityScale(T left, T right)
+        {
+            this.right = right;
+            this.left = left;
+        }
+
+        public T AreEqual()
+        {
+            var comparison = left.CompareTo(right);
+
+            if (comparison > 0)
+            {
+                return left;
+            }
+            else if (comparison < 0)
+            {
+                return right;
+            }
+
+            return default(T);
+        }
+    }
+}
